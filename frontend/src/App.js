533731a88ec52466cb107a7670ccf4b59f5dab73
Ryan Breaux <ryanbreaux@Ryans-iMac.local>
2019-08-01 20:25:29 -0700

    inital commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MyDropzone from "./upload";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <MyDropzone />
+    </div>
+  );
+}
+
+export default App;
