51a0ceeffdbfe3fcdf67fd6e17c035faf5d477a9
Ryan Breaux <ryanbreaux@Ryans-iMac.local>
2019-08-28 10:21:22 -0700

    Animations for movie page

--- frontend/src/pages/movies.js
+++ frontend/src/pages/movies.js
@@ -1,11 +1,17 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Upload, VideoPlayer } from "../components/upload";
 import { Box, Image, Flex, Button, Card, Heading, Text } from "rebass";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 import genres from "../library/genres.json";
 import gql from "graphql-tag";
-import { useSpring, animated, interpolate } from "react-spring";
-import ImageFilter from "react-image-filter";
+import {
+  useSpring,
+  useTransition,
+  useTrail,
+  animated,
+  interpolate
+} from "react-spring";
+import styled from "styled-components";
 
 const queryMovies = gql`
   query getMovies($input: String) {
@@ -52,9 +58,10 @@ const MoviePreview = ({ playMovie, data, handleClick }) => {
   return (
     <Container
       borderRadius="3px"
-      maxWidth="20vw"
       ref={containerRef}
-      onClick={() => handleClick(data)}
+      onClick={() => {
+        return handleClick(data);
+      }}
       onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
       onMouseLeave={() => set({ xys: [0, 0, 1] })}
       className="animated fadeIn"
@@ -75,24 +82,78 @@ const MoviePreview = ({ playMovie, data, handleClick }) => {
 };
 
 const MainPreview = ({ img, Year, Plot, Poster, Awards, Title }) => {
+  const ImageContainer = animated(styled(Image)`
+    &::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(rgb(2, 2, 2) 60%, rgba(0, 0, 0, 0));
+    }
+  `);
+
+  const [loading, setLoad] = useState(true);
+  useEffect(() => {
+    loading && setLoad(!loading);
+  }, [loading]);
+
+  const transitions = useTransition(loading, null, {
+    from: { opacity: 0, transform: "translateX(-100%)" },
+    enter: { opacity: 1, transform: "translateX(0)" },
+    leave: { opacity: 1, transform: "translateX(0)" }
+  });
+
   return (
     <Flex
       style={{
         height: "60vh",
         width: "100%",
-        backgroundColor: "black",
-        zIndex: 2
+        zIndex: 2,
+        position: "absolute",
+        top: 0,
+        background: "linear-gradient(rgb(2, 2, 2) 60%, rgba(0, 0, 0, 0))"
       }}
     >
-      <Box>
-        <Heading p="3em">{Title}</Heading>
-        <Card color="white">
-          <Text>{Year}</Text>
-          <Text px="8em">{Plot}</Text>
-          <Text>{Awards}</Text>
+      <Box p="3em">
+        <Heading color="#d6d6d6" p="1em" m="3em" mb="1em">
+          {Title}
+        </Heading>
+        <hr style={{ color: "white", width: "40%" }} />
+        <Card color="white" fontSize=".8em">
+          <Text px="8em" pb="3em">
+            {Plot}
+          </Text>
+          <Text textAlign="center" fontSize=".4em">
+            {Year}
+            {Awards}
+          </Text>
         </Card>
       </Box>
-      <Image width={[1 / 2]} co src={Poster} alt="" />
+      <Card
+        width={["50vw"]}
+        style={{
+          right: 0,
+          top: 0,
+          zIndex: -1,
+          height: "100%",
+          position: "absolute"
+        }}
+      >
+        {transitions.map(
+          ({ item, key, props }) =>
+            item && (
+              <ImageContainer
+                key={key}
+                borderRadius={"6px"}
+                m="3em"
+                style={props}
+                src={Poster}
+              />
+            )
+        )}
+      </Card>
     </Flex>
   );
 };
@@ -105,8 +166,6 @@ const Slider = ({ handleClick, category }) => {
   } = useQuery(queryMovies, {
     variables: { input: category }
   });
-
-  !loading && console.log(data, category, error);
 
   return (
     <>
@@ -147,13 +206,22 @@ export default function Movies() {
   const [upload, toggleUpload] = useState(false);
   const [player, togglePlayer] = useState(false);
   const [inspect, toggleInspect] = useState(false);
-
+  const [isTop, checkPosition] = useState(false);
+  const scrollRef = useRef(null);
   const categories = genres;
 
+  useEffect(() => {
+    console.log(window);
+    window.addEventListener("scroll", pos => {
+      console.log(pos);
+    });
+    return () => {};
+  }, [isTop]);
+
   return (
-    <Box>
-      <MainPreview {...inspect} />
-      <Box>
+    <Box ref={scrollRef}>
+      <MainPreview welcome={isTop} {...inspect} />
+      <Box pt="60vh">
         {categories.map((category, i) => {
           return (
             <Slider
