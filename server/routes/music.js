40051160c0c53e02e1899b3fa1cf05f81b8daf66
Ryan Breaux <ryanbreaux@ryans-imac.lan>
2020-06-09 02:59:03 -0700

    Added storage on server

--- server/routes/music.js
+++ server/routes/music.js
@@ -6,42 +6,38 @@ const axio = require("axios");
 const musicRoutes = express.Router();
 
 const scripts = {
-  recording: path.basename("../workers/setupRecording.sh")
+  recording: path.basename("../workers/setupRecording.sh"),
 };
 
 const musicSockets = (socket, stream, database) => {
   stream.emit("startCapture", (stream, data) => {
-    // const displays = execFile("../workers/setupRecording.sh", ["dev"], {
-    //   deatached: true
+    // // const displays = execFile("../workers/setupRecording.sh", ["dev"], {
+    // //   deatached: true
+    // // });
+    // // displays.stdout.on("data", data => {
+    // //   console.log(data.toString());
+    // // });
+    // const child = spawn(`ffmpeg`, [
+    //   "-f",
+    //   "avfoundation",
+    //   "-i",
+    //   "1|1",
+    //   "output.mkv",
+    // ]);
+    // child.on("exit", (code) => {
+    //   console.log(`Child process exited with code ${code}`);
     // });
-    // displays.stdout.on("data", data => {
-    //   console.log(data.toString());
+    // child.stdout.on("data", (data) => {
+    //   const recording = path.basename(`../output.mkv`);
+    //   const screenCapture = fs.createReadStream(recording);
+    //   screenCapture.pipe(stream);
     // });
-
-    const child = spawn(`ffmpeg`, [
-      "-f",
-      "avfoundation",
-      "-i",
-      "1|1",
-      "output.mkv"
-    ]);
-
-    child.on("exit", code => {
-      console.log(`Child process exited with code ${code}`);
-    });
-    child.stdout.on("data", data => {
-      const recording = path.basename(`../output.mkv`);
-      const screenCapture = fs.createReadStream(recording);
-      screenCapture.pipe(stream);
-    });
-    child.stderr.on("data", data => {
-      console.log(`stderr: ${data}`);
-    });
+    // child.stderr.on("data", (data) => {
+    //   console.log(`stderr: ${data}`);
+    // });
   });
 };
 
-musicRoutes.get("/", (req, res) => {
-  console.log(req);
-});
+musicRoutes.get("/", (req, res) => {});
 
 module.exports = { musicRoutes, musicSockets };
